07f31ba722f3735b891809e159f93fc973e3dadb
Victor Gabriel del Rosario <victordelrosariogalva@gmail.com>
2023-02-21 17:12:19 -0400

    Syling
    Fix Some Styling Issues

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 import {View, StyleSheet, Text,Image} from 'react-native';
-let imgSize = 100;
+let imgSize = 120;
 
 const ItemList = ({movie}) => {
+    const [pressedColor,setPressedColor]= useState('container');
     const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
     const{title,overview,release_date,original_title,original_language,adult,vote_average,vote_count,poster_path} = movie;
     return (
-        <View style = {styles.container}>
+        <View style = {styles[pressedColor]}>
             <Image 
                 source={{uri: IMAGE_PATH + poster_path}}
                 alt = {title}
@@ -14,7 +16,7 @@ const ItemList = ({movie}) => {
             /> 
             <View style = {styles.itemInfo}>
                 <Text style = {styles.itemTitle}>{title}</Text>
-                <Text>{release_date}</Text>
+                <Text style = {styles.itemReleaseDate}>{release_date}</Text>
                 <Text style={styles.itemDescription}>{overview}</Text>
                 <Text style={styles.itemRating}>{vote_average}</Text>
             </View>
@@ -27,12 +29,24 @@ const styles = StyleSheet.create({
     container:{
         overflow:'hidden',
         padding:10,
-        marginVertical:5,
+        //marginVertical:5,
         width:'100%',
-        height:120,
+        height:160,
         backgroundColor:'#FFFFFF',
-        borderRadius:20,
-        flexDirection:'row'
+        //borderRadius:20,
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    containerPressed:{
+        overflow:'hidden',
+        padding:10,
+        //marginVertical:5,
+        width:'100%',
+        height:160,
+        backgroundColor:'orange',
+        //borderRadius:20,
+        flexDirection:'row',
+        alignItems:'center',
     },
     imgs:{
         width:imgSize,
@@ -46,6 +60,9 @@ const styles = StyleSheet.create({
     itemInfo:{
         marginLeft:10,
 
+    },
+    itemReleaseDate:{
+        color:"#999999",
     },
     itemDescription:{
         height:40,
